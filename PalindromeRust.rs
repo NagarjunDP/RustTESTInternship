b10664525e0f0d6b8ca5e4c72a13a8c28fab8df2
NagarjunDP <147615489+NagarjunDP@users.noreply.github.com>
2024-06-07 12:51:57 +0530

    Update PalindromeRust.rs

--- PalindromeRust.rs
+++ PalindromeRust.rs
@@ -1,16 +1,16 @@
 use std::io;
 
 fn main() {
-
+    //Input from user
     println!("Enter the string: ");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
-  
+    // Trim the input to remove any trailing newline character and convert to lowercase
     let input = input.trim().to_lowercase();
 
     let reversed_input: String = input.chars().rev().collect();
-
+    //Checking if input is equal to reversed string
     if input == reversed_input {
         println!("It is a PALINDROME!!!");
     } else {
