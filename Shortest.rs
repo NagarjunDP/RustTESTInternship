8b7567ec109c27b88d3f055fec3f4f0af68ee671
NagarjunDP <147615489+NagarjunDP@users.noreply.github.com>
2024-06-07 13:14:15 +0530

    Update Shortest.rs

--- Shortest.rs
+++ Shortest.rs
@@ -1,9 +1,13 @@
-words = "Hey I am Intern at QuadB Tech Company "
+fn main() {
+    let words = "Hey I am Intern at QuadB Tech Company ";
+    let word: Vec<&str> = words.trim().split_whitespace().collect();
+    let mut shortest = word[0];
 
-word = words.split()
-shortest = word[0]
-for i in word[1:]: 
-    if len(i) < len(shortest):  
-        shortest = i
+    for i in &word[1..] {
+        if i.len() < shortest.len() {
+            shortest = i;
+        }
+    }
 
-print(shortest)
+    println!("{}", shortest);
+}
